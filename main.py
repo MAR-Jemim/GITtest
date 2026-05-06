d270acd8becebc1772af085591b034aee2d5b0d4
MAR-Jemim <marjemim966@gmail.com>
2026-05-06 19:17:02 +0600

    Commit 2

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
     print("I am testing git and github")
+    print("I am commiting something")
 
 
 
